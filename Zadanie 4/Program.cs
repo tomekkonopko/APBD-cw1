001783226cfe7fa191f4373aed09b3b0a3a54339
Tomasz Konopko <s18902@pjwstk.edu.pl>
2021-03-22 00:16:30 +0100

    Zadanie Numer 4

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Zadanie_4
+{
+    class Program
+    {
+        public async static System.Threading.Tasks.Task Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0) throw new ArgumentException("Parametr nie został podany");
+
+                if (!Uri.IsWellFormedUriString(args[0], UriKind.Absolute))
+                    throw new ArgumentException("Adres URL jest błędny");
+
+                var httpClient = new HttpClient();
+                var url = args[0];
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var htmlContent = await response.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(htmlContent);
+
+                    foreach (var match in matches)
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                string blad = string.Format("Napotkalem blad", exc.ToString());
+                Console.WriteLine("Tresc bledu: " + exc.ToString());
+            }
+
+        }
+    }
+}
