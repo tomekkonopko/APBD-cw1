304cc63ec2d96c6081c7f6c7490b41619569177c
Tomasz Konopko <s18902@pjwstk.edu.pl>
2021-03-22 00:14:48 +0100

    Zadanie Clawler

---
+++ Zadanie Clawler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Zadanie_Clawler
+{
+    class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+
+        }
+    }
+}
